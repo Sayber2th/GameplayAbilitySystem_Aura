b74bdc5775968a3b766e7d017682f6c5470512b2
Omkar Rajesh Kulkarni <63438170+Sayber2th@users.noreply.github.com>
2023-09-22 13:44:37 +0530

    Highlight enemies

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "Interaction/EnemyInterface.h"
 
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
@@ -30,6 +31,14 @@ void AAuraPlayerController::BeginPlay()
     InputModeData.SetHideCursorDuringCapture(false);
     SetInputMode(InputModeData);
 }
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+    Super::PlayerTick(DeltaTime);
+
+    CursorTrace();
+}
+
 void AAuraPlayerController::SetupInputComponent()
 {
     Super::SetupInputComponent();
@@ -52,4 +61,62 @@ void AAuraPlayerController::Move(const FInputActionValue &InputActionValue)
         ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
         ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
     }
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+    FHitResult CursorHit;
+    GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, CursorHit);
+    if(!CursorHit.bBlockingHit) return;
+
+    LastActor = ThisActor;
+    ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+    /**
+     * Line trace from cursor. There are several scenarios:
+     * A. LastActor is null && ThisActor is null
+     *      -Do nothing
+     * B. LastActor is null && ThisActor is valid
+     *      -Highlight ThisActor
+     * C. LastActor is valid && ThisActor is null
+     *      -Unhighlight the LastActor
+     * D. Both actors are valid; LastActor != ThisActor
+     *      -Unhighlight LastActor & Highlight ThisActor
+     * E. Both actors are valid; LastActor == ThisActor
+     *      -Do nothing
+    */
+
+   if(LastActor == nullptr)
+   {
+        if(ThisActor != nullptr)
+        {
+            //Case B
+            ThisActor->HighlightActor();
+        }
+        else
+        {
+            //Case A - do nothing
+        }
+   }
+   else //LastActor is valid
+   {
+        if(ThisActor == nullptr)
+        {
+            //Case C
+            LastActor->UnHighlightActor();
+        }
+        else //Both actors are valid
+        {
+            if(LastActor != ThisActor)
+            {
+                //Case D
+                LastActor->UnHighlightActor();
+                ThisActor->HighlightActor();
+            }
+            else
+            {
+                //Case E - Do nothing
+            }
+        }
+   }
 }
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -10,6 +10,7 @@
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
+class IEnemyInterface;
 
 /**
  * 
@@ -21,6 +22,7 @@ class AURA_API AAuraPlayerController : public APlayerController
 	
 public:
 	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
 
 protected:
 	virtual void BeginPlay() override;
@@ -34,5 +36,9 @@ private:
 	TObjectPtr<UInputAction> MoveAction;
 
 	void Move(const FInputActionValue& InputActionValue);
+
+	void CursorTrace();
+	IEnemyInterface* ThisActor;
+	IEnemyInterface* LastActor;
 
 };
