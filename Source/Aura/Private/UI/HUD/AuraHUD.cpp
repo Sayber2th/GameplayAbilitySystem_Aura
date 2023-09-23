9a40976b7398eecc5894c5cccd1297b116678554
Omkar Rajesh Kulkarni <63438170+Sayber2th@users.noreply.github.com>
2023-09-23 16:13:38 +0530

    Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,13 @@
+// Copyright Omkar Kulkarni
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+    Super::BeginPlay();
+
+    UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+    Widget->AddToViewport();
+}
