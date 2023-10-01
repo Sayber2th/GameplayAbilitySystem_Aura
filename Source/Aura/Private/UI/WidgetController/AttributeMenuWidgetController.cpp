a527f3537ab09ffcfd6db871da4839d398051a06
Omkar Rajesh Kulkarni <63438170+Sayber2th@users.noreply.github.com>
2023-10-01 17:55:43 +0530

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Omkar Kulkarni
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+    
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
