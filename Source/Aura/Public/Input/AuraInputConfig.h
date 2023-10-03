aeb4cd6513b49b5a41056d0ea91179470716598c
Omkar Rajesh Kulkarni <63438170+Sayber2th@users.noreply.github.com>
2023-10-03 17:53:50 +0530

    Input Config Data Asset

---
+++ Source/Aura/Public/Input/AuraInputConfig.h
@@ -0,0 +1,35 @@
+// Copyright Omkar Kulkarni
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "AuraInputConfig.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraInputAction
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	const class UInputAction* InputAction = nullptr;
+
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag InputTag = FGameplayTag();
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	const UInputAction* FindAbilityInputAction(const FGameplayTag& InputTag, bool bLogNotFound = false) const;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FAuraInputAction> AbilityInputActions;
+};
