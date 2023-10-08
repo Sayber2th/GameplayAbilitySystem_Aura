39c2d2ac789a0a6e8c897e8426290c1a219e1644
Omkar Rajesh Kulkarni <63438170+Sayber2th@users.noreply.github.com>
2023-10-08 10:14:59 +0530

    Implementing Auto Running

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -45,6 +45,24 @@ void AAuraPlayerController::PlayerTick(float DeltaTime)
     Super::PlayerTick(DeltaTime);
 
     CursorTrace();
+    AutoRun();
+}
+
+void AAuraPlayerController::AutoRun()
+{
+    if(!bAutoRunning) return;
+    if(APawn* ControlledPawn = GetPawn())
+    {
+        const FVector LocationOnSpline = Spline->FindLocationClosestToWorldLocation(ControlledPawn->GetActorLocation(), ESplineCoordinateSpace::World);
+        const FVector Direction = Spline->FindDirectionClosestToWorldLocation(LocationOnSpline, ESplineCoordinateSpace::World);
+        ControlledPawn->AddMovementInput(Direction);
+
+        const float DistanceToDestination = (LocationOnSpline - CachedDestination).Length();
+        if(DistanceToDestination <= AutoRunAcceptanceRadius)
+        {
+            bAutoRunning = false;
+        }
+    }
 }
 
 void AAuraPlayerController::SetupInputComponent()
@@ -170,6 +188,7 @@ void AAuraPlayerController::AbilityInputTagReleased(FGameplayTag InputTag)
                     Spline->AddSplinePoint(PointLoc, ESplineCoordinateSpace::World);
                     DrawDebugSphere(GetWorld(), PointLoc, 8.f, 8, FColor::Green, false, 5.f);
                 }
+                CachedDestination = NavPath->PathPoints[NavPath->PathPoints.Num() - 1];
                 bAutoRunning = true;
             }
         }
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -68,4 +68,6 @@ private:
 
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USplineComponent> Spline;
+
+	void AutoRun();
 };
