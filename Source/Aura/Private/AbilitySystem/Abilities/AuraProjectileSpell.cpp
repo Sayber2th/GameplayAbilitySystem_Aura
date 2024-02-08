5fba3393adfa004778731695dd3ca6cdab33b011
Omkar Rajesh Kulkarni <63438170+Sayber2th@users.noreply.github.com>
2024-02-08 11:38:00 +0530

    Revert "The Gameplay Effect Context"
    This reverts commit 892669540292ee0075fc65eb8685151fcdffdf77.

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -39,17 +39,7 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
             ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
 
         const UAbilitySystemComponent* SourceASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
-        FGameplayEffectContextHandle EffectContextHandle = SourceASC->MakeEffectContext();
-        EffectContextHandle.SetAbility(this);
-        EffectContextHandle.AddSourceObject(Projectile);
-        TArray<TWeakObjectPtr<AActor>> Actors;
-        Actors.Add(Projectile);
-        EffectContextHandle.AddActors(Actors);
-        FHitResult HitResult;
-        HitResult.Location = ProjectileTargetLocation;
-        EffectContextHandle.AddHitResult(HitResult);
-
-        const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), EffectContextHandle);
+        const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
         
         const FAuraGameplayTags GameplayTags = FAuraGameplayTags::Get();
         const float ScaledDamage = Damage.GetValueAtLevel(10);
