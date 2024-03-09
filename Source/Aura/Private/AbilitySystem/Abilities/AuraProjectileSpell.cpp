5eb67ee380c3b6de8a7e7848ab4ded99f3b87b15
Omkar Rajesh Kulkarni <63438170+Sayber2th@users.noreply.github.com>
2024-03-09 12:46:30 +0530

    Lec 156 & Hit Message

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -54,8 +54,6 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
         const FAuraGameplayTags GameplayTags = FAuraGameplayTags::Get();
         const float ScaledDamage = Damage.GetValueAtLevel(10);
         
-        GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, FString::Printf(TEXT("FireBolt Damage: %f"), ScaledDamage));
-        
         UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Damage, ScaledDamage);
         Projectile->DamageEffectSpecHandle = SpecHandle;
 
