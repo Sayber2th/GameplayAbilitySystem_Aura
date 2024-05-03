dae5e085aa9698b1c1266c48feb27dc4424dcbd2
Omkar Rajesh Kulkarni <63438170+Sayber2th@users.noreply.github.com>
2024-05-03 13:37:08 +0530

    Translucent Actors upon Camera Occlusion

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -1,4 +1,7 @@
 // Copyright Omkar Kulkarni
+//Camera Occlusion code taken from Alfred Baudisch's github :- https://gist.github.com/alfredbaudisch/7978f1913e76640e2dc25b770ffa6ae1
+//Link to Alfred Baudisch's blog used to create the camera occlusion effect to fade out actors in between camera and character :-
+//https://alfredbaudisch.com/blog/gamedev/unreal-engine-ue/unreal-engine-actors-transparent-block-camera-occlusion-see-through/
 
 
 #include "Player/AuraPlayerController.h"
@@ -13,9 +16,20 @@
 #include "NavigationPath.h"
 #include "GameFramework/Character.h"
 #include "UI/Widget/DamageTextComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Containers/Set.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
+    /* Camera Occlusion variables */
+
+    CapsulePercentageForTrace = 1.0f;
+    DebugLineTraces = true;
+    IsOcclusionEnabled = true;
+
+    /* Camera Occlusion variables */
+
     bReplicates = true;
     Spline = CreateDefaultSubobject<USplineComponent>("Spline");
 }
@@ -39,6 +53,14 @@ void AAuraPlayerController::BeginPlay()
     InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
     InputModeData.SetHideCursorDuringCapture(false);
     SetInputMode(InputModeData);
+
+    if (IsValid(GetPawn()))
+    {
+        ActiveSpringArm = Cast<USpringArmComponent>(GetPawn()->GetComponentByClass(USpringArmComponent::StaticClass()));
+        ActiveCamera = Cast<UCameraComponent>(GetPawn()->GetComponentByClass(UCameraComponent::StaticClass()));
+        ActiveCapsuleComponent = Cast<UCapsuleComponent>(GetPawn()->GetComponentByClass(UCapsuleComponent::StaticClass()));
+    }
+
 }
 
 void AAuraPlayerController::PlayerTick(float DeltaTime)
@@ -198,4 +220,150 @@ UAuraAbilitySystemComponent* AAuraPlayerController::GetASC()
     }
 
     return AuraAbilitySystemComponent;
+}
+
+void AAuraPlayerController::SyncOccludedActors()
+{
+    if (!ShouldCheckCameraOcclusion()) return;
+
+    // Camera is currently colliding, show all current occluded actors
+    // and do not perform further occlusion
+    if (ActiveSpringArm->bDoCollisionTest)
+    {
+        ForceShowOccludedActors();
+        return;
+    }
+
+    FVector Start = ActiveCamera->GetComponentLocation();
+    FVector End = GetPawn()->GetActorLocation();
+
+    TArray<TEnumAsByte<EObjectTypeQuery>> CollisionObjectTypes;
+    CollisionObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_WorldStatic));
+
+    TArray<AActor*> ActorsToIgnore; // TODO: Add configuration to ignore actor types
+    TArray<FHitResult> OutHits;
+
+    auto ShouldDebug = DebugLineTraces ? EDrawDebugTrace::ForDuration : EDrawDebugTrace::None;
+
+    bool bGotHits = UKismetSystemLibrary::CapsuleTraceMultiForObjects(
+        GetWorld(), Start, End, ActiveCapsuleComponent->GetScaledCapsuleRadius() * CapsulePercentageForTrace,
+        ActiveCapsuleComponent->GetScaledCapsuleHalfHeight() * CapsulePercentageForTrace, CollisionObjectTypes, true,
+        ActorsToIgnore,
+        ShouldDebug,
+        OutHits, true);
+
+    if (bGotHits)
+    {
+        // The list of actors hit by the line trace, that means that they are occluded from view
+        TSet<const AActor*> ActorsJustOccluded;
+
+        // Hide actors that are occluded by the camera
+        for (FHitResult Hit : OutHits)
+        {
+            const AActor* HitActor = Cast<AActor>(Hit.GetActor());
+            HideOccludedActor(HitActor);
+            ActorsJustOccluded.Add(HitActor);
+        }
+
+        // Show actors that are currently hidden but that are not occluded by the camera anymore 
+        for (auto& Elem : OccludedActors)
+        {
+            if (!ActorsJustOccluded.Contains(Elem.Value.Actor) && Elem.Value.IsOccluded)
+            {
+                ShowOccludedActor(Elem.Value);
+
+                if (DebugLineTraces)
+                {
+                    UE_LOG(LogTemp, Warning,
+                        TEXT("Actor %s was occluded, but it's not occluded anymore with the new hits."), *Elem.Value.Actor->GetName());
+                }
+            }
+        }
+    }
+    else
+    {
+        ForceShowOccludedActors();
+    }
+}
+
+bool AAuraPlayerController::HideOccludedActor(const AActor* Actor)
+{
+    FCameraOccludedActor* ExistingOccludedActor = OccludedActors.Find(Actor);
+
+    if (ExistingOccludedActor && ExistingOccludedActor->IsOccluded)
+    {
+        if (DebugLineTraces) UE_LOG(LogTemp, Warning, TEXT("Actor %s was already occluded. Ignoring."),
+            *Actor->GetName());
+        return false;
+    }
+
+    if (ExistingOccludedActor && IsValid(ExistingOccludedActor->Actor))
+    {
+        ExistingOccludedActor->IsOccluded = true;
+        OnHideOccludedActor(*ExistingOccludedActor);
+
+        if (DebugLineTraces) UE_LOG(LogTemp, Warning, TEXT("Actor %s exists, but was not occluded. Occluding it now."), *Actor->GetName());
+    }
+    else
+    {
+        UStaticMeshComponent* StaticMesh = Cast<UStaticMeshComponent>(
+            Actor->GetComponentByClass(UStaticMeshComponent::StaticClass()));
+
+        FCameraOccludedActor OccludedActor;
+        OccludedActor.Actor = Actor;
+        OccludedActor.StaticMesh = StaticMesh;
+        OccludedActor.Materials = StaticMesh->GetMaterials();
+        OccludedActor.IsOccluded = true;
+        OccludedActors.Add(Actor, OccludedActor);
+        OnHideOccludedActor(OccludedActor);
+
+        if (DebugLineTraces) UE_LOG(LogTemp, Warning, TEXT("Actor %s does not exist, creating and occluding it now."), *Actor->GetName());
+    }
+
+    return true;
+}
+
+
+void AAuraPlayerController::ForceShowOccludedActors()
+{
+    for (auto& Elem : OccludedActors)
+    {
+        if (Elem.Value.IsOccluded)
+        {
+            ShowOccludedActor(Elem.Value);
+
+            if (DebugLineTraces) UE_LOG(LogTemp, Warning, TEXT("Actor %s was occluded, force to show again."), *Elem.Value.Actor->GetName());
+        }
+    }
+}
+
+void AAuraPlayerController::ShowOccludedActor(FCameraOccludedActor& OccludedActor)
+{
+    if (!IsValid(OccludedActor.Actor))
+    {
+        OccludedActors.Remove(OccludedActor.Actor);
+    }
+
+    OccludedActor.IsOccluded = false;
+    OnShowOccludedActor(OccludedActor);
+}
+
+bool AAuraPlayerController::OnShowOccludedActor(const FCameraOccludedActor& OccludedActor) const
+{
+    for (int matIdx = 0; matIdx < OccludedActor.Materials.Num(); ++matIdx)
+    {
+        OccludedActor.StaticMesh->SetMaterial(matIdx, OccludedActor.Materials[matIdx]);
+    }
+
+    return true;
+}
+
+bool AAuraPlayerController::OnHideOccludedActor(const FCameraOccludedActor& OccludedActor) const
+{
+    for (int i = 0; i < OccludedActor.StaticMesh->GetNumMaterials(); ++i)
+    {
+        OccludedActor.StaticMesh->SetMaterial(i, FadeMaterial);
+    }
+
+    return true;
 }
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -1,10 +1,16 @@
 // Copyright Omkar Kulkarni
+//Camera Occlusion code taken from Alfred Baudisch's github :- https://gist.github.com/alfredbaudisch/7978f1913e76640e2dc25b770ffa6ae1
+//Link to Alfred Baudisch's blog used to create the camera occlusion effect to fade out actors in between camera and character :-
+//https://alfredbaudisch.com/blog/gamedev/unreal-engine-ue/unreal-engine-actors-transparent-block-camera-occlusion-see-through/
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "GameplayTagContainer.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "AuraPlayerController.generated.h"
 
 
@@ -16,6 +22,28 @@ class IEnemyInterface;
 class UAuraInputConfig;
 class UAuraAbilitySystemComponent;
 class USplineComponent;
+
+/* Camera Occlusion */
+
+USTRUCT(BlueprintType)
+struct FCameraOccludedActor
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	const AActor* Actor;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TArray<UMaterialInterface*> Materials;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	bool IsOccluded = false;
+};
+
+/* Camera Occlusion */
 
 /**
  * 
@@ -35,6 +63,36 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
+
+	/* Camera Occlusion */
+
+	/** How much of the Pawn capsule Radius and Height
+	* should be used for the Line Trace before considering an Actor occluded?
+	* Values too low may make the camera clip through walls.
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera Occlusion|Occlusion",
+		meta = (ClampMin = "0.1", ClampMax = "10.0"))
+	float CapsulePercentageForTrace;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera Occlusion|Materials")
+	UMaterialInterface* FadeMaterial;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Camera Occlusion|Components")
+	class USpringArmComponent* ActiveSpringArm;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Camera Occlusion|Components")
+	class UCameraComponent* ActiveCamera;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Camera Occlusion|Components")
+	class UCapsuleComponent* ActiveCapsuleComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera Occlusion")
+	bool IsOcclusionEnabled;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera Occlusion|Occlusion")
+	bool DebugLineTraces;
+
+	/* Camera Occlusion */
 
 private:
 	UPROPERTY(EditAnywhere, Category = Input)
@@ -85,4 +143,31 @@ private:
 
 	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<UDamageTextComponent> DamageTextComponentClass;
+
+	/* Camera Occlusion */
+
+	TMap<const AActor*, FCameraOccludedActor> OccludedActors;
+
+	bool HideOccludedActor(const AActor* Actor);
+	bool OnHideOccludedActor(const FCameraOccludedActor& OccludedActor) const;
+	void ShowOccludedActor(FCameraOccludedActor& OccludedActor);
+	bool OnShowOccludedActor(const FCameraOccludedActor& OccludedActor) const;
+	void ForceShowOccludedActors();
+
+	__forceinline bool ShouldCheckCameraOcclusion() const
+	{
+		return IsOcclusionEnabled && FadeMaterial && ActiveCamera && ActiveCapsuleComponent;
+	}
+
+	/* Camera Occlusion */
+
+public:
+
+	/* Camera Occlusion */
+
+	UFUNCTION(BlueprintCallable)
+	void SyncOccludedActors();
+	
+	/* Camera Occlusion */
+
 };
